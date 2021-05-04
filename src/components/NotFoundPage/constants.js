6dc45774075370f3442ccf44d51a1d5f4d45ec37
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-05-05 01:30:56 +0300

    Feat: 404 page

---
+++ src/components/NotFoundPage/constants.js
@@ -0,0 +1,2 @@
+import pageNotFound from '../../images/page_not_found.jpg';
+export const pageNotFoundImg = pageNotFound;
---
+++ src/components/NotFoundPage/texts.js
@@ -0,0 +1,1 @@
+export const pageNotFoundTitle = "Упс..кажется здесь ничего нет!";
