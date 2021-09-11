b4d07af974cb8cc2909dff6dbe96008b2428250a
Zhanna Anikanova <Zhanna.anikanova@unrvld.com>
2021-09-11 12:21:02 +0300

    Config: resolve prettier/eslinter conflicts

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -9,19 +9,39 @@ export default function AboutPage() {
   return (
     <section className='about-page'>
       <h2 className='about-page__title'>О бренде</h2>
-      {
-        ABOUT.map((item, i) => (
-          (i + 1) % 2 === 1
-            ? <div className='about-page__group' key={i}>
-              <StyledText text={item} styles={{ default: { className: 'about-page__text' }, colored: { className: 'about-page__text_colored' } }}/>
-              <img src={photoAbout[i]} className='about-page__img' alt={`О бренде. Изображение ${i}`}/>
-            </div>
-            : <div className='about-page__group' key={i}>
-              <img src={photoAbout[i]} className='about-page__img' alt={`О бренде. Изображение ${i}`}/>
-              <StyledText text={item} styles={{ default: { className: 'about-page__text' }, colored: { className: 'about-page__text_colored' } }}/>
-            </div>
-        ))
-      }
+      {ABOUT.map((item, i) =>
+        (i + 1) % 2 === 1 ? (
+          <div className='about-page__group' key={i}>
+            <StyledText
+              text={item}
+              styles={{
+                default: { className: 'about-page__text' },
+                colored: { className: 'about-page__text_colored' },
+              }}
+            />
+            <img
+              src={photoAbout[i]}
+              className='about-page__img'
+              alt={`О бренде. Изображение ${i}`}
+            />
+          </div>
+        ) : (
+          <div className='about-page__group' key={i}>
+            <img
+              src={photoAbout[i]}
+              className='about-page__img'
+              alt={`О бренде. Изображение ${i}`}
+            />
+            <StyledText
+              text={item}
+              styles={{
+                default: { className: 'about-page__text' },
+                colored: { className: 'about-page__text_colored' },
+              }}
+            />
+          </div>
+        ),
+      )}
     </section>
   );
 }
