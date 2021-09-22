006329a27d1fd8544470f615a2e5dd74d25bbaf3
Zhanna Anikanova <whiteredfoxx@gmail.com>
2021-09-23 00:10:45 +0300

    feat: create TermsCheckbox component

--- src/components/BasketForm/BasketForm.js
+++ src/components/BasketForm/BasketForm.js
@@ -1,16 +1,18 @@
 import './BasketForm.css';
 import Button from '../Button/Button';
+import TermsCheckbox from '../TermsCheckbox/TermsCheckbox';
 
 function BasketForm() {
   return (
     <form className='basket-page__form'>
       <fieldset className='basket-page__fieldset'>
-        <input type='tel' className='basket-page__tel' placeholder='+7 (___)-___-__-__' />
+        <input required type='tel' className='basket-page__tel' placeholder='+7 (___)-___-__-__' />
         <textarea
           className='basket-page__address'
           placeholder='Адрес (улица, дом, квартира / офис)'
         />
       </fieldset>
+      <TermsCheckbox />
       <Button type='submit' text='Оформить заказ' btnStyle='button_type_order' />
     </form>
   );
